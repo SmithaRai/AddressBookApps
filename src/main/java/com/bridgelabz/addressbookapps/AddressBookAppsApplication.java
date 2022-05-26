1d457e4920437a75980907e0332debfeac7db7f7
unknown <SmithaRai.com>
2022-05-26 12:49:40 +0530

    Added : added logging setup

--- src/main/java/com/bridgelabz/addressbookapps/AddressBookAppsApplication.java
+++ src/main/java/com/bridgelabz/addressbookapps/AddressBookAppsApplication.java
@@ -1,13 +1,16 @@
 package com.bridgelabz.addressbookapps;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
+@Slf4j
 public class AddressBookAppsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AddressBookAppsApplication.class, args);
-    }
+       ApplicationContext context = SpringApplication.run(AddressBookAppsApplication.class, args);
 
+  log.info("Employee Payroll App Started in {} Environment", context.getEnvironment().getProperty("environment"));}
 }
